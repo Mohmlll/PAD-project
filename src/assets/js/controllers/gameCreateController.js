9edebed0cf465e76c08c8cd6739011a34cba83e5
Mohammed Malloul <mohammed.malloul@hva.nl>
2021-04-14 16:11:25 +0200

    styling create spel

--- src/assets/js/controllers/gameCreateController.js
+++ src/assets/js/controllers/gameCreateController.js
@@ -17,6 +17,7 @@ class GameCreateController {
             contentType: "application/json",
             method: "get"
         });
+
         this.intDropDownData = this.dropDownData.length;
 
         for (let i = 0; i < this.intDropDownData; i++) {
@@ -38,15 +39,14 @@ class GameCreateController {
             contentType: "application/json",
             method: "get"
         });
+
         this.intDropDownDataGameType = this.dropDownDataGameType.length;
 
         for (let i = 0; i < this.intDropDownDataGameType; i++) {
-            let option = $('<option class="gameType"></option>').attr("value", "option value");
+            let option = $("<option></option>");
             option.text(this.dropDownDataGameType[i]["type"])
-            option.appendTo("#gametype");
+            option.appendTo("#game_type");
         }
-
-
     }
 
     async onAddGame() {
@@ -100,9 +100,9 @@ class GameCreateController {
             let materialTemplateUsable = $(materialTemplate);
 
             materialTemplateUsable.find(".labelMaterials").text(material);
-            materialTemplateUsable.find("#noOfRoom").attr("id", "noOfRoom" + material_id);
-            materialTemplateUsable.find("#adds").attr("id", "adds" + material_id);
-            materialTemplateUsable.find("#subs").attr("id", "subs" + material_id);
+            materialTemplateUsable.find(".material-input").attr("id", "noOfRoom" + material_id);
+            materialTemplateUsable.find(".material-plus").attr("id", "adds" + material_id);
+            materialTemplateUsable.find(".material-minus").attr("id", "subs" + material_id);
             materialTemplateUsable.appendTo("#materialview");
 
         }
