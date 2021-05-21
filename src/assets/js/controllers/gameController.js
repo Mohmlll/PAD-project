8fa53f6317b3f2e43b807c29369c04d923de1d28
Mohammed Malloul <mohammed.malloul@hva.nl>
2021-05-21 12:49:17 +0200

    Alleen materiaal moet nog gedaan worden met filteren

--- src/assets/js/controllers/gameController.js
+++ src/assets/js/controllers/gameController.js
@@ -35,7 +35,7 @@ class GameController {
             materialTemplateUsable.find(".material-input").attr("id", "noOfRoom" + material_id);
             materialTemplateUsable.find(".material-plus").attr("id", "adds" + material_id);
             materialTemplateUsable.find(".material-minus").attr("id", "subs" + material_id);
-            materialTemplateUsable.appendTo("#materialview");
+            materialTemplateUsable.appendTo("#materialview2");
 
         }
 
@@ -57,6 +57,118 @@ class GameController {
                 gameType = row.type;
                 this.fillTemplate(gameTemplate, name, gameId);
             }
+        }
+    }
+
+
+    fillTemplate(gameTemplate, name, gameId) {
+        let gameRowTemplate = $(gameTemplate);
+        gameRowTemplate.find(".game-name").text(name);
+
+        gameRowTemplate.on("click", () => {
+            this.navigateTo(gameId);
+        })
+        gameRowTemplate.find("a[href='.collapseSummary']").attr('href', '.collapseSummary' + gameId);
+        gameRowTemplate.appendTo("#gameview");
+
+
+    }
+
+    navigateTo(gameId) {
+        let url = window.location.href;
+        app.loadController(CONTROLLER_GAME_INFO);
+        // location.replace(url + "game" + gameId);
+        location.replace(url + gameId);
+    }
+
+    async filter() {
+        let gameType, minAudience, maxAudience, amountStudents, filteredTypeGames;
+        let game = this.game;
+        let allSorts = "Alle soorten";
+
+        amountStudents = $("#student-amount").val();
+        gameType = $("#game-type-filter").val();
+        minAudience = $("#game-target-audience-min").val();
+        maxAudience = $("#game-target-audience-max").val();
+
+
+        if (gameType === allSorts) {
+            filteredTypeGames = game.filter(function (e) {
+                return (e["target_audience_min"] >= minAudience) && (e["target_audience_max"] <= maxAudience) && (e["amount_players"] >= amountStudents);
+
+            });
+        } else {
+            filteredTypeGames = game.filter(function (e) {
+                return (e.type === gameType) && (minAudience >= e["target_audience_min"]) && (maxAudience <= e["target_audience_max"]) && (e["amount_players"] >= amountStudents);
+
+            });
+        }
+
+        console.log(filteredTypeGames);
+
+        if (filteredTypeGames.length === 0) {
+            $('.no-result-alert').show();
+        } else {
+            $('.no-result-alert').hide();
+        }
+
+        await this.onGetGame(filteredTypeGames)
+    }
+
+
+    async getDropDownDataGameTypeFilter() {
+        // get data
+        this.dropDownDataGameTypeFilter = await $.ajax({
+            url: baseUrl + "/gametype",
+            contentType: "application/json",
+            method: "get"
+        });
+
+        this.intDropDownDataGameTypeFilter = this.dropDownDataGameTypeFilter.length;
+
+        for (let i = 0; i < this.intDropDownDataGameTypeFilter; i++) {
+            $('#game-type-filter').append(`<option value="${this.dropDownDataGameTypeFilter[i]["type"]}">
+            ${this.dropDownDataGameTypeFilter[i]["type"]} </option>`)
+        }
+
+    }
+
+    async getDropDownDataGameAudienceFilter() {
+        // get data
+        this.dropDownDataGameAudienceFilter = await $.ajax({
+            url: baseUrl + "/audience",
+            contentType: "application/json",
+            method: "get"
+        });
+        let optionMinS
+        for (let i = 0; i < this.dropDownDataGameAudienceFilter.length; i++) {
+            if (this.dropDownDataGameAudienceFilter[i]["audience"] === 0) {
+                optionMinS = "Alle jaarlagen";
+            } else {
+                optionMinS = "Groep " + this.dropDownDataGameAudienceFilter[i]["audience"];
+            }
+
+            let optionMin = $('<option class="audienceMin"></option>').attr("value", this.dropDownDataGameAudienceFilter[i]["audience"]);
+            let optionMax = $('<option class="audienceMax"></option>').attr("value", this.dropDownDataGameAudienceFilter[i]["audience"]);
+
+            optionMin.text(optionMinS);
+            optionMax.text(optionMinS);
+            optionMin.appendTo("#game-target-audience-min");
+            optionMax.appendTo("#game-target-audience-max");
+        }
+    }
+
+    async getDropDownGameMaterialFilter() {
+        // get data
+        this.dropDownDataGameMaterialFilter = await $.ajax({
+            url: baseUrl + "/material",
+            contentType: "application/json",
+            method: "get"
+        });
+        this.intDropDownDataGameMaterialFilter = this.dropDownDataGameMaterialFilter.length;
+        for (let i = 0; i < this.intDropDownDataGameMaterialFilter; i++) {
+            $('#game-material-filter').append(`<option value="${this.dropDownDataGameMaterialFilter[i]["material"]}">
+            ${this.dropDownDataGameMaterialFilter[i]["material"]}</option>`)
         }
     }
 
@@ -92,132 +204,25 @@ class GameController {
         }
     }
 
-
-    fillTemplate(gameTemplate, name, gameId) {
-        let gameRowTemplate = $(gameTemplate);
-        gameRowTemplate.find(".game-name").text(name);
-
-        gameRowTemplate.on("click", () => {
-            this.navigateTo(gameId);
-        })
-        gameRowTemplate.find("a[href='.collapseSummary']").attr('href', '.collapseSummary' + gameId);
-        gameRowTemplate.appendTo("#gameview");
-
-
-    }
-
-    navigateTo(gameId) {
-        let url = window.location.href;
-        app.loadController(CONTROLLER_GAME_INFO);
-        // location.replace(url + "game" + gameId);
-        location.replace(url + gameId);
-    }
-
-
-    async getDropDownDataGameTypeFilter() {
-        // get data
-        this.dropDownDataGameTypeFilter = await $.ajax({
-            url: baseUrl + "/gametype",
-            contentType: "application/json",
-            method: "get"
-        });
-
-        this.intDropDownDataGameTypeFilter = this.dropDownDataGameTypeFilter.length;
-
-        for (let i = 0; i < this.intDropDownDataGameTypeFilter; i++) {
-            $('#game-type-filter').append(`<option value="${this.dropDownDataGameTypeFilter[i]["type"]}">
-            ${this.dropDownDataGameTypeFilter[i]["type"]} </option>`)
-        }
-
-    }
-
-    async getDropDownDataGameAudienceFilter() {
-        // get data
-        this.dropDownDataGameAudienceFilter = await $.ajax({
-            url: baseUrl + "/audience",
-            contentType: "application/json",
-            method: "get"
-        });
-        let optionMinS
-        for (let i = 0; i <  this.dropDownDataGameAudienceFilter.length; i++) {
-            if (this.dropDownDataGameAudienceFilter[i]["audience"] === 0) {
-                optionMinS = "Alle jaarlagen";
-            } else {
-                optionMinS = "Groep " + this.dropDownDataGameAudienceFilter[i]["audience"];
-            }
-
-            let optionMin = $('<option class="audienceMin"></option>').attr("value", this.dropDownDataGameAudienceFilter[i]["audience"]);
-            let optionMax = $('<option class="audienceMax"></option>').attr("value", this.dropDownDataGameAudienceFilter[i]["audience"]);
-
-            optionMin.text(optionMinS);
-            optionMax.text(optionMinS);
-            optionMin.appendTo("#game-target-audience-min");
-            optionMax.appendTo("#game-target-audience-max");
-        }
-    }
-
-    async getDropDownGameMaterialFilter() {
-        // get data
-        this.dropDownDataGameMaterialFilter = await $.ajax({
-            url: baseUrl + "/material",
-            contentType: "application/json",
-            method: "get"
-        });
-        this.intDropDownDataGameMaterialFilter = this.dropDownDataGameMaterialFilter.length;
-        for (let i = 0; i < this.intDropDownDataGameMaterialFilter; i++) {
-            $('#game-material-filter').append(`<option value="${this.dropDownDataGameMaterialFilter[i]["material"]}">
-            ${this.dropDownDataGameMaterialFilter[i]["material"]}</option>`)
-        }
-    }
-
-    async filter() {
-        let gameType, minLeerjaar, maxLeerjaar;
-        let filteredTypeGames;
-        let game = this.game;
-
-        gameType = $("#game-type-filter").val();
-        minLeerjaar = $("#game-target-audience-min").val();
-        maxLeerjaar = $("#game-target-audience-max").val();
-
-        filteredTypeGames = game.filter(function (e) {
-            return (e.type === gameType) && (e["target_audience_min"] >= minLeerjaar) && (e["target_audience_max"] <= maxLeerjaar);
-
-        });
-        console.log(filteredTypeGames);
-        await this.onGetGame(filteredTypeGames)
-    }
-
-
     //Called when the home.html has been loaded
     async setup(data) {
         //Load the welcome-content into memory
         this.gameView = $(data);
 
-        $('#search-filter', this.gameView).on("keyup", async (e) => {
-            const searchString = e.target.value.toLowerCase();
-
-            const filteredGames = this.game.filter((game) => {
-                return (
-                    game.name.toLowerCase().includes(searchString)
-                );
-            });
-            console.log(filteredGames);
-            await this.onGetGame(filteredGames);
-            if (filteredGames.length === 0) {
-                $('.no-result-alert').show();
-            } else {
-                $('.no-result-alert').hide();
-            }
-
-        });
 
         $("#filter-button", this.gameView).on("click", () => {
             this.filter()
         });
+        $("#student-amount", this.gameView).on("input", () => {
+            const slider = document.getElementById("student-amount");
+            const output = document.getElementById("student-amount-value");
+            output.innerHTML = slider.value; // Display the default slider value
 
-        $('#inputFilter', this.gameView).on("keyup", () => {
-            this.filter()
-        })
+
+            slider.oninput = function () {
+                output.innerHTML = this.value;
+            }
+        });
 
         //Empty the content-div and add the resulting view to the page
         $(".content").empty().append(this.gameView);
