16cbd3c43ed16442ee38715cbba6bd92f7bf46fe
Yusuf Kara <yusuf.kara@hva.nl>
2021-05-26 14:09:08 +0200

    adminpanel changes

--- src/assets/js/controllers/adminController.js
+++ src/assets/js/controllers/adminController.js
@@ -6,4 +6,17 @@ class AdminController {
             .then((data) => this.setup(data))
             .catch(() => this.error());
     }
+
+    //Called when the home.html has been loaded
+    async setup(data) {
+
+        //Load the welcome-content into memory
+        this.adminView = $(data);
+        this.adminView.find("[data-controller]").on("click", NavbarController.handleClickMenuItem);
+
+        //Empty the content-div and add the resulting view to the page
+        $(".content").empty().append(this.adminView);
+        // listen for redirects
+        templateManager.listen();
+    }
 }
