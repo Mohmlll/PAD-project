a689d5b520f953c2109db3dcb3361fdc4fd7044d
Yusuf Kara <yusuf.kara@hva.nl>
2021-06-04 15:48:38 +0200

    message error - not done

--- src/assets/js/utils/message.js
+++ src/assets/js/utils/message.js
@@ -1,25 +1,34 @@
-
 /**
- * Implementation of a template manager that handles ppage  events
+ * Implementation of a template manager that handles page  events
  */
 const DEFAULT_SUCCESS_MESSAGE = `Actie uitgevoerd.`
 const DEFAULT_ERROR_MESSAGE = `Er is iets fout gegaan.`
+const DEFAULT_WARNING_MESSAGE = `Er is een fout opgetreden.`
 
 const message = {
-    success(message = DEFAULT_SUCCESS_MESSAGE){
+    success(message = DEFAULT_SUCCESS_MESSAGE) {
         $.notify({
             // options
             message: message
-        },{
+        }, {
             // settings
             type: 'success'
         });
     },
-    error(message = DEFAULT_ERROR_MESSAGE){
+    error(message = DEFAULT_ERROR_MESSAGE) {
         $.notify({
             // options
             message: message
-        },{
+        }, {
+            // settings
+            type: 'error'
+        });
+    },
+    warning(message = DEFAULT_WARNING_MESSAGE) {
+        $.notify({
+            // options
+            message: message
+        }, {
             // settings
             type: 'warning'
         });
