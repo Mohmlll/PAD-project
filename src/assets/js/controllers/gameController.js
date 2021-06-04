836cebc8a0fa6efc2f2e92d0cf0b6305c1d2b9db
Yusuf Kara <yusuf.kara@hva.nl>
2021-06-04 16:12:08 +0200

    gamerating spelpagina

--- src/assets/js/controllers/gameController.js
+++ src/assets/js/controllers/gameController.js
@@ -23,8 +23,9 @@ class GameController {
                 gameId = row["id_game"];
                 name = row["name"];
                 gameType = row["type"];
+                let avgRating = await this.getAvgRating(gameId) || 0
 
-                gameRows = gameRows.add(this.fillTemplate(gameTemplate, name, gameId, row));
+                gameRows = gameRows.add(this.fillTemplate(gameTemplate, name, avgRating, gameId, row));
             }
         } else {
             for (let i = 0; i < games.length; i++) {
@@ -32,7 +33,9 @@ class GameController {
                 gameId = row.id_game;
                 name = row.name;
                 gameType = row.type;
-                gameRows = gameRows.add(this.fillTemplate(gameTemplate, name, gameId, row));
+                let avgRating = await this.getAvgRating(gameId) || 0
+
+                gameRows = gameRows.add(this.fillTemplate(gameTemplate, name, avgRating, gameId, row));
             }
         }
 
@@ -74,9 +77,10 @@ class GameController {
         }
     }
 
-    fillTemplate(gameTemplate, name, gameId, games) {
+    fillTemplate(gameTemplate, name, avgRating, gameId, games) {
         let gameRowTemplate = $(gameTemplate);
-        gameRowTemplate.find(".game-name").text(name);
+        gameRowTemplate.find(".game-name").text(name + " (" + avgRating + "☆)");
+
         gameRowTemplate.find(".game-image").attr('src', 'uploads/' + games['game_icon']);
         gameRowTemplate.on("click", async () => {
             let userId = sessionManager.get("id");
@@ -87,6 +91,12 @@ class GameController {
         gameRowTemplate.find("a[href='.collapseSummary']").attr('href', '.collapseSummary' + gameId);
         return gameRowTemplate;
 
+    }
+
+    async getAvgRating(gameId) {
+        let avgRatingCall = await this.statRepository.getAvgRatingForSpecifiedGame(gameId)
+        this.avgRating = avgRatingCall.data[0]["average"];
+        return avgRatingCall.data[0]["average"];
     }
 
     async click(userId, gameId, click) {
@@ -260,7 +270,7 @@ class GameController {
                 $('#adds' + i).parent().prev().children().val(Number(curr) + 1)
             });
         }
-    } 
+    }
 
     remove() {
         for (let i = 1; i <= this.result.length; i++) {
