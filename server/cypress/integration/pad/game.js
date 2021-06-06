1f17919ad2aab096de22c594f5e28ef2fd95bf27
Devran Ilhan <Devran_ilhan@live.nl>
2021-06-06 19:34:23 +0200

    cypress test uitgebreid

--- server/cypress/integration/pad/game.js
+++ server/cypress/integration/pad/game.js
@@ -7,35 +7,73 @@ describe("GameCreate", function () {
     });
 
     //Test: Validate create game form
-    it("Valid login form", function() {
-        //Find the field for the username, description and the amount of students -> check if it exists.
+    it("Valid game create form", function () {
+        //Find the field for the username, description, amount of players, game icon and game plan -> check if it exists.
         cy.get('input[name="name"]').should("exist");
         cy.get('input[name="description"]').should("exist");
-        cy.get('input[name="amount-students"]').should("exist");
+        cy.get('input[name="min-players"]').should("exist");
+        cy.get('input[name="game-icon"]').should("exist");
+        cy.get('input[name="game-plan"]').should("exist");
 
-        //Find the field for the target audience and check if it exists
+        //Find the field for the target audience -> check if it exists
         cy.get("#target-audience-min").should("exist");
         cy.get("#target-audience-max").should("exist");
 
-        //Find the field for the type of game -> check if it exists
-        cy.get("#gametype").should("exist");
+        //Find the field for the game rules -> check if it exists
+        cy.get("#game-rules").should("exist");
 
-        //Find the button to login -> check if it exists.
-        cy.get("#form-submit").should("exist");
-
-        //Find the field for the type of material, substraction and addition button -> check if it exists
+        //Find the fields for the material input -> check if they exist
         for (let i = 1; i < 9; i++) {
+            cy.get("#subs" + i).should("exist");
             cy.get("#noOfRoom" + i).should("exist");
             cy.get("#adds" + i).should("exist");
-            cy.get("#subs" + i).should("exist");
         }
 
+        //Find the field for the type of game -> check if it exists
+        cy.get("#game_type").should("exist");
+
+        //Find the button to submit the game create form -> check if it exists.
+        cy.get("#game").should("exist");
+
+    });
+
+    //Test: Failed to create game
+    it("Failed to create game", () => {
+        //Start a fake server
+        cy.server();
+
+        //Add a stub with the URL /game as a POST
+        //Respond with a JSON-object when requested and set the status-code tot 401.
+        //Give the stub the alias: @gameCreate
+        cy.route({
+            method: "POST",
+            url: "/game",
+            response: {
+                reason: "ERROR"
+            },
+            status: 401
+        }).as("gameCreate");
+
+        //Find the field for the name and type the text "test".
+        cy.get('input[name="name"]').type("test");
+
+        //Find the field for the description and type the text "test".
+        cy.get('input[name="description"]').type("test");
+
+        //Find the button to login and click it.
+        cy.get("#game").click();
+
+        //Wait for the @gameCreate-stub to be called by the click-event.
+        cy.wait("@gameCreate");
+
+        //After a failing to create a game, an element containing our error-message should be shown.
+        cy.get(".error").should("exist").should("contain", "ERROR");
     });
 
     // describe("GameCreate", () => {
     //     beforeEach(() => {
     //         cy.visit("http://localhost:8080/#gameCreate");
-    //         const session = {"username": "jim"};
+    //         const session = {"email": "root", "id": 87};
     //         localStorage.setItem("session", JSON.stringify(session));
     //     });
     //     it("Shows all pokemon from request", () => {
@@ -55,36 +93,4 @@ describe("GameCreate", function () {
     //     });
     // });
 
-    // //Test: Failed login
-    // it("Failed login", function () {
-    //     //Start a fake server
-    //     cy.server();
-    //
-    //     //Add a stub with the URL /user/login as a POST
-    //     //Respond with a JSON-object when requested and set the status-code tot 401.
-    //     //Give the stub the alias: @login
-    //     cy.route({
-    //         method: "POST",
-    //         url: "/user/login",
-    //         response: {
-    //             reason: "ERROR"
-    //         },
-    //         status: 401
-    //     }).as("login");
-    //
-    //     //Find the field for the username and type the text "test".
-    //     cy.get("#exampleInputUsername").type("test");
-    //
-    //     //Find the field for the password and type the text "test".
-    //     cy.get("#exampleInputPassword").type("test");
-    //
-    //     //Find the button to login and click it.
-    //     cy.get(".login-form button").click();
-    //
-    //     //Wait for the @login-stub to be called by the click-event.
-    //     cy.wait("@login");
-    //
-    //     //After a failed login, an element containing our error-message should be shown.
-    //     cy.get(".error").should("exist").should("contain", "ERROR");
-    // });
 });
